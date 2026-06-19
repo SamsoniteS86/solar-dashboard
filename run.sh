a064c8bbb61bc0d866897bf67ed6a69cd01badf4
SamsoniteS86 <71758592+SamsoniteS86@users.noreply.github.com>
2026-06-19 10:22:03 +0200

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,12 +1,14 @@
-#!/usr/bin/with-contenv bashenv
+#!/bin/bash
 
 CONFIG_PATH=/data/options.json
 
-export INVERTER_IP=$(jq -r '.inverter_ip' $CONFIG_PATH)
-export INVERTER_PORT=$(jq -r '.inverter_port' $CONFIG_PATH)
-export IHOME_IP=$(jq -r '.ihome_ip' $CONFIG_PATH)
-export IHOME_PORT=$(jq -r '.ihome_port' $CONFIG_PATH)
-export DEMO_MODE=$(jq -r '.demo_mode' $CONFIG_PATH)
+if [ -f "$CONFIG_PATH" ]; then
+  export INVERTER_IP=$(jq -r '.inverter_ip' $CONFIG_PATH)
+  export INVERTER_PORT=$(jq -r '.inverter_port' $CONFIG_PATH)
+  export IHOME_IP=$(jq -r '.ihome_ip' $CONFIG_PATH)
+  export IHOME_PORT=$(jq -r '.ihome_port' $CONFIG_PATH)
+  export DEMO_MODE=$(jq -r '.demo_mode' $CONFIG_PATH)
+fi
 
 cd /app
 exec node server.js
